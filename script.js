af01ee4d799ef518a49906a4c7801e56bd6b6393
Matheus Nunes <matheusjobs04@gmail.com>
2025-03-22 16:13:24 -0300

    atualizações no JS

--- script.js
+++ script.js
@@ -16,25 +16,30 @@ function relogioUpdate() {
 
     // Atualizar os elementos HTML
     document.getElementById('clock').textContent = formatTempo;
-    document.querySelector('.topics').textContent = `${day} de ${month} de ${year}`;
+    document.getElementById('dia-mes-ano').textContent = `${day} de ${month} de ${year}`;
 
     // Trocar o fundo conforme as horas
     trocarBG(hours);
 }
 
 function trocarBG(hours) {
-    let backColor;
+    let backColor, boxColor;
 
     if (hours >= 6 && hours < 13) {
-        backColor = "linear-gradient(to bottom, #A0C7F4 0%, #B2D5FD 44%, #BCDBFF 87%)"; // Manhã
+        backColor = "linear-gradient(to bottom, #A0C7F4 0%, #B2D5FD 44%, #BCDBFF 87%)";
+        boxColor = "linear-gradient(to bottom, #A0C7F4 40%, #BCDBFF 80%)"; // Manhã
     } else if (hours >= 13 && hours < 18) {
-        backColor = "linear-gradient(to bottom, #E5D272 0%, #FF9A76 44%, #8669EE 87%)"; // Tarde
+        backColor = "linear-gradient(to bottom, #E5D272 0%, #FF9A76 44%, #8669EE 87%)";
+        boxColor = "linear-gradient(to bottom, #E5D272 0%, #C884AD 46%,  #8669EE 100%)";
+         // Tarde
     } else {
-        backColor = "linear-gradient(to bottom, #0B063A 0%, #2D2197 44%, #5A4DC8 87%)"; // Noite
+        backColor = "linear-gradient(to bottom, #0B063A 0%, #2D2197 44%, #5A4DC8 87%)"; 
+        boxColor = "linear-gradient(to bottom, #5A4DC8 0%, #2D2197 100%)"; // Noite
     }
 
     // Aplicar o gradiente ao body
     document.body.style.background = backColor;
+    document.querySelector('.box').style.background = boxColor;
 }
 
 // Atualizar o relógio a cada segundo
